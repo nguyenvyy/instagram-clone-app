ad2b47b55d1f72cc1258d3c0e483ad4897b2e2ab
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-05-18 07:27:08 +0700

    upddate RouteWithAuth

--- src/custom-routes/RouteWithAuth.js
+++ src/custom-routes/RouteWithAuth.js
@@ -1,22 +1,26 @@
-import React from 'react'
-import { useStore } from '../hooks/useStore'
-import { Redirect, Route } from 'react-router-dom'
-import { useRouter } from '../hooks/useRouter'
+import React from 'react';
+import { useStore } from '../hooks/useStore';
+import { Redirect, Route } from 'react-router-dom';
+import { useRouter } from '../hooks/useRouter';
+import { getCookie } from '../services/storage';
+import { env } from '../config/globals';
+import { FullLoading } from '../components/common/FitLoading';
 
-export const RouteWithAuth = ({
-    redirectPath = '/sign-in',
-    path,
-    children, 
-    ...rest}) => {
-    const store = useStore()
-    const router = useRouter()
-
-    return store.auth.isAuthenticated === true ? (
-        <Route path={path} {...rest}>
-            {children}
-        </Route>
-    ) : (
-        <Redirect to={{pathname: redirectPath, state: {from: router.pathname}}} />
-    )
-
-}
+export const RouteWithAuth = ({ redirectPath = '/sign-in', path, children, ...rest }) => {
+	const store = useStore();
+	const router = useRouter();
+	const existedToken = getCookie(env.COOKIE_KEY);
+	if (store.auth.isAuthenticated) {
+		return (
+			<Route path={path} {...rest}>
+				{children}
+			</Route>
+		);
+    }
+    if(existedToken && store.auth.initLoading === true) {
+        return <FullLoading />
+	}
+	
+	const from = router.pathname
+	return <Redirect to={{ pathname: redirectPath, state: { from } }} />;
+};
